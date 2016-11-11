36117591ef154c378f6b4cca25b23285b00dfa52
solmi <thfadl@ajou.ac.kr>
2016-11-11 17:09:37 +0900

    검색 수평리스트, 알림리스트

---
+++ app/src/main/java/kr/co/trappan/Activity/SearchActivity.java
@@ -0,0 +1,18 @@
+package kr.co.trappan.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+
+import kr.co.trappan.R;
+
+/**
+ * Created by thfad_000 on 2016-11-11.
+ */
+
+public class SearchActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search);
+    }
+}
