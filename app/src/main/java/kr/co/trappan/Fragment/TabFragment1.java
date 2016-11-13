2a47d1884d3f292090f4d942b2839f841459613b
SeungJun <daekwan6@naver.com>
2016-11-13 20:38:51 +0900

    TapFragment1 수정

--- app/src/main/java/kr/co/trappan/Fragment/TabFragment1.java
+++ app/src/main/java/kr/co/trappan/Fragment/TabFragment1.java
@@ -9,6 +9,7 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 
 import kr.co.trappan.Adapter.Horizontal_Pager_Adapter;
@@ -19,6 +20,7 @@ public class TabFragment1 extends Fragment {
 
 
    private ViewPager horizontal_viewpager;
+    private RelativeLayout item1,item2,item3;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -27,6 +29,14 @@ public class TabFragment1 extends Fragment {
         horizontal_viewpager=(ViewPager)view.findViewById(R.id.horizontal_viewpager);
         Horizontal_Pager_Adapter adapter=new Horizontal_Pager_Adapter(getLayoutInflater(savedInstanceState));
         horizontal_viewpager.setAdapter(adapter);
+
+        item1=(RelativeLayout)view.findViewById(R.id.item1);
+        item2=(RelativeLayout)view.findViewById(R.id.item2);
+        item3=(RelativeLayout)view.findViewById(R.id.item3);
+
+        item1.setBackgroundResource(R.drawable.c);
+        item2.setBackgroundResource(R.drawable.d);
+        item3.setBackgroundResource(R.drawable.e);
 
         return view;
     }
