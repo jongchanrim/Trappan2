d691a78a395055e6b8d51f92b420f018a9881357
SeungJun <daekwan6@naver.com>
2016-11-17 01:52:12 +0900

    후기페이지 완료

---
+++ app/src/main/java/kr/co/trappan/Adapter/ReviewPagerAdapter.java
@@ -0,0 +1,72 @@
+package kr.co.trappan.Adapter;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import kr.co.trappan.R;
+
+/**
+ * Created by SeungJun on 16. 11. 9..
+ */
+public class ReviewPagerAdapter extends PagerAdapter{
+
+    LayoutInflater inflater;
+
+    public ReviewPagerAdapter(LayoutInflater inflater) {
+        this.inflater=inflater;
+    }
+
+
+    @Override
+    public int getCount() {
+        return 6;
+    }
+
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        View view = null;
+        view = inflater.inflate(R.layout.horizontal_viewpager, null);
+
+        ImageView img = (ImageView) view.findViewById(R.id.viewpager_image);
+
+        if (position == 0) {
+            img.setBackgroundResource(R.drawable.a);
+        } else if (position == 1) {
+            img.setBackgroundResource(R.drawable.seoul);
+        } else if (position == 2) {
+            img.setBackgroundResource(R.drawable.back_button);
+        } else if (position == 3) {
+            img.setBackgroundResource(R.drawable.detail_icon_01_02);
+        } else if (position == 4) {
+            img.setBackgroundResource(R.drawable.detail_icon_04_a0203);
+        } else if (position == 5) {
+            img.setBackgroundResource(R.drawable.seoul);
+        }
+            //img.setImageResource(R.drawable.a);
+
+            container.addView(view);
+
+            return view;
+        }
+
+
+        @Override
+        public void destroyItem (ViewGroup container,int position, Object object){
+
+            container.removeView((View) object);
+
+        }
+
+
+        @Override
+        public boolean isViewFromObject (View v, Object obj){
+            // TODO Auto-generated method stub
+            return v == obj;
+        }
+
+}
